9fcd4e973cb1e7289d56b55a7f33d7d8879e7071
diego <d.josesandoval@hotmail.com>
2022-08-16 00:17:21 -0600

    added vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="/css/app.css" rel="stylesheet">
+  @vite('resources/css/app.css')
   <link rel="icon" type="image/x-icon" href="../Diego_logo.ico">
   <title>Diego Sandoval</title>
 </head>
