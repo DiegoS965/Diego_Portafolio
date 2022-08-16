ec9a19c0d723ad39836c2355c324d3a9aed37fab
diego <d.josesandoval@hotmail.com>
2022-08-16 00:41:37 -0600

    modified service provider

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @production
   <link rel="stylesheet" href="{{ secure_asset('/css/app.css') }}">
+  @endproduction
   <link rel="icon" type="image/x-icon" href="../Diego_logo.ico">
   <title>Diego Sandoval</title>
 </head>
